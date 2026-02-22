5af876b8ecf71a63d814a2468004f770f4804c32
Seidy <seidyalanis@gmail.com>
2026-02-22 16:15:27 -0600

    Conexión a base de datos MongoDB.

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/ticoAuto');
+
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+database.once('connected', () => {
+    console.log('Conexión a la BD exitosa');
+});
+
+
 
 const app = express();
 
