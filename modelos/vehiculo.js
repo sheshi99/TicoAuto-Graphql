4bcfb281cbcae5ec14649db2e5d1936018f5f503
Seidy <seidyalanis@gmail.com>
2026-02-22 16:37:31 -0600

    Creación del modelo de vehículo.

---
+++ modelos/vehiculo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const vehiculoSchema = new mongoose.Schema({
+
+    marca: {
+        type: String,
+        required: true
+    },
+
+    modelo: {
+        type: String,
+        required: true
+    },
+
+    anno: {
+        type: Number,
+        required: true
+    },
+
+    precio: {
+        type: Number,
+        required: true
+    },
+
+    // En estado, puede ser "disponible" o "vendido". Por defecto será "disponible"
+    estado: {
+        type: String,
+        enum: ['disponible', 'vendido'],
+        default: 'disponible'
+    }
+
+});
+
+module.exports = mongoose.model('Vehiculo', vehiculoSchema);
