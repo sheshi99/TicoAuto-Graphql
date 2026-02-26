ee73b6389401921a794ee3f17c4026cc5f3f16d7
walbyn <walbyngonzalez@gmail.com>
2026-02-26 10:23:45 -0600

    Se crea la carpeta de la ruta de autenticacion y se agrega la ruta de registro de usuario

---
+++ rutas/autenticacion.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const { registrarUsuario } = require('../controladores/autenticacion');
+
+// Ruta para registrar un nuevo usuario
+router.post('/registrar', registrarUsuario);
+
+module.exports = router;
