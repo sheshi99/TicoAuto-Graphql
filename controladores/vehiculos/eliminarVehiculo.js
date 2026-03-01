cba58a38d33018df0bf4aa04c93340a90a11c5b5
walbyn <walbyngonzalez@gmail.com>
2026-03-01 17:30:43 -0600

    Se crea el archivo y se translada método

---
+++ controladores/vehiculos/eliminarVehiculo.js
@@ -0,0 +1,18 @@
+const Vehiculo = require("../../modelos/vehiculo");
+
+const eliminarVehiculo = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deletedVehiculo = await Vehiculo.findByIdAndDelete(id);
+        if (!deletedVehiculo) {
+            return res.status(404);
+        }
+        res.status(200).json(deletedVehiculo);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    eliminarVehiculo
+};
