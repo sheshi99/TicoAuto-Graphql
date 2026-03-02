2a8b7132542e234c8f6bb31db08e62d5cacbf237
walbyn <walbyngonzalez@gmail.com>
2026-03-01 18:22:53 -0600

    Se modifico el metodo para que elimine la imagen (solo si, se edita la imagen)

--- controladores/vehiculos/editarVehiculo.js
+++ controladores/vehiculos/editarVehiculo.js
@@ -1,4 +1,6 @@
 const Vehiculo = require("../../modelos/vehiculo");
+const fs = require('fs'); 
+
 
 const editarVehiculo = async (req, res) => {
     try {
@@ -7,10 +9,16 @@ const editarVehiculo = async (req, res) => {
 
         const imagen = req.file; // Verificar si se ha subido una nueva imagen
         if (imagen) {
-            // Actualizar el campo de imagen con el nuevo nombre de archivo
+            // Si el vehículo ya tiene una imagen, eliminar la imagen anterior del servidor
+            const vehiculoExistente = await Vehiculo.findById(id); 
+            if (vehiculoExistente && vehiculoExistente.imagen) {
+                const rutaImagenAnterior = `imagenes/${vehiculoExistente.imagen}`;
+                if (fs.existsSync(rutaImagenAnterior)) { 
+                    fs.unlinkSync(rutaImagenAnterior); 
+                }
+            }
             body.imagen = req.file.filename;
         }
-
         const opciones = { new: true }; 
         const updatedVehiculo = await Vehiculo.findByIdAndUpdate(id, body, opciones);
         if (!updatedVehiculo) { 
