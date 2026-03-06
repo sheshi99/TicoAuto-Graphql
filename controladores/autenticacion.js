89998712145f334de245458e6bf163e5bf417507
walbyn <walbyngonzalez@gmail.com>
2026-03-06 15:21:56 -0600

    Se detecta un error en la palabra require

--- controladores/autenticacion.js
+++ controladores/autenticacion.js
@@ -1,6 +1,6 @@
 const usuario = require('../modelos/usuario');
-const jwt = requiere('jsonwebtoken');
-const bcrypt = requiere('bcrypt');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const SECRET_KEY = process.env.JWT_SECRET;
 
