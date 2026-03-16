1d420201e9b6c308287cec4743f21ac958596d78
walbyn <walbyngonzalez@gmail.com>
2026-03-15 18:21:01 -0600

    Se agregan validaciones de los campos

--- modelos/vehiculo.js
+++ modelos/vehiculo.js
@@ -1,68 +1,60 @@
 const mongoose = require('mongoose');
 
 const vehiculoSchema = new mongoose.Schema({
-
     marca: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
-
     modelo: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    anno: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    precio: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    imagen: {
         type: String,
         required: true
     },
-
-    anno: {
-        type: Number,
-        required: true
-    },
-
-    precio: {
-        type: Number,
-        required: true
-    },
-
-    imagen: { 
-        type: String, 
-        required: true 
-    },  
-
-    // En estado, puede ser "disponible" o "vendido". Por defecto será "disponible"
     estado: {
         type: String,
         enum: ['disponible', 'vendido'],
         default: 'disponible'
     },
-
     combustible: {
         type: String,
         enum: ['Gasolina', 'Disel', 'Gas'],
         required: true
     },
-
-    color:{
+    color: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
-
     transmision: {
         type: String,
-        enum: ['Manual', 'Automatico']
+        enum: ['Manual', 'Automatico'],
+        required: true
     },
-
     condicion: {
         type: String,
-        enum: ['Nuevo','Usado']
+        enum: ['Nuevo', 'Usado'],
+        required: true
     },
-
-    usuario: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Usuario', 
-        required: true 
+    usuario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: true
     }
-
-},
-
-{ timestamps: true });
+}, { timestamps: true });
 
 module.exports = mongoose.model('Vehiculo', vehiculoSchema);
