d006a1d2a2ad0311d50d11f2d504e3b1a47772b9
walbyn <walbyngonzalez@gmail.com>
2026-04-02 00:51:32 -0600

    Se cambia el id_client

--- utilidades/verificarGoogle.js
+++ utilidades/verificarGoogle.js
@@ -1,6 +1,6 @@
 const { OAuth2Client } = require('google-auth-library');
 
-const CLIENT_ID = '252538192927-4h489g0gsg982vn2lpsh6ek63kv5a8vb.apps.googleusercontent.com';
+const CLIENT_ID = '817077174471-bt5e6bh7eelg0989ksook2k9m1eu9tbp.apps.googleusercontent.com';
 const client = new OAuth2Client(CLIENT_ID);
 
 const verificarGoogleToken = async (credential) => {
