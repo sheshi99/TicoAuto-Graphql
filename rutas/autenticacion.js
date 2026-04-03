3ee9503df737df60391fd783545ff75bbc6444dd
Seidy <seidyalanis@gmail.com>
2026-04-02 20:20:34 -0600

    Unifica las rutas de autenticación con autenticacion de Google. Así mismo, se elimina archivo y ruta de autenticación google.

--- rutas/autenticacion.js
+++ rutas/autenticacion.js
@@ -2,16 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 
+// Autenticación local
 const {registrarUsuario } = require('../controladores/autenticacion/registrarUsuario');
 const {generarToken} = require('../controladores/autenticacion/generarToken');
 const {verificarCorreo} = require('../controladores/autenticacion/verificarCorreo');
   
-        
-// Ruta para registrar un nuevo usuario
-router.post('/autenticacion', registrarUsuario);
 
+// Autenticación con Google
+const { registrarUsuarioGoogle } = require('../controladores/autenticacion/registroGoogle');
+const { loginGoogle } = require('../controladores/autenticacion/loginGoogle');
+
+
+// Rutas de atenticación local
+router.post('/autenticacion', registrarUsuario);
 router.post('/autenticacion/login', generarToken);
 
+
+// Verificación de correo
 router.get('/autenticacion/verificacion', verificarCorreo);
+
+
+// Rutas de autenticación con Google
+router.post('/autenticacion/google', registrarUsuarioGoogle);
+router.post('/autenticacion/google/login', loginGoogle);
+
 
 module.exports = router;
